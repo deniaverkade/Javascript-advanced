5542055b865b5b9abbc66d6c22c4e75cd3d9d3a7
deniaverkade <9024406@student.zadkine.nl>
2025-09-19 15:04:50 +0200

    4.2

--- 1-js-methods/level-4-local-storage/4.2-todo-lijst/app.js
+++ 1-js-methods/level-4-local-storage/4.2-todo-lijst/app.js
@@ -4,10 +4,10 @@ console.log('🚀 Opdracht 4.2: Todo Lijst');
 let taken = []; // Array van strings (geen objecten!)
 
 function voegTaakToe() {
-    const input = document.getElementById('nieuwe-taak');
-    const taakTekst = localStorage.setItem("taak", input);
+    const input = document.getElementById('nieuwe-taak').value;
+   
     
-    if (taakTekst === '') {
+    if (input === '') {
         alert('Voer een taak in!');
         return;
     }
@@ -17,7 +17,7 @@ function voegTaakToe() {
     
     // TODO: Sla taken op in localStorage
     /* jouw code hier - gebruik JSON.stringify */
-    let stringifyTaak = localStorage.setItem("opgeslagenTaak",JSON.stringify(input));
+    localStorage.setItem("opgeslagenTaak",JSON.stringify(input));
     
     // Maak input leeg en update UI
 
@@ -45,7 +45,7 @@ function toonTaken() {
     // TODO: Genereer HTML voor alle taken
     const takenHTML = taken.map((taak, index) => `
         <div class="taak">
-            ${taakTekst}
+            ${taak}
             <button onclick="verwijderTaak(${index})">🗑️</button>
         </div>
     `).join('');
@@ -53,22 +53,22 @@ function toonTaken() {
     container.innerHTML = takenHTML;
     
     // TODO: Update teller
-    document.getElementById('aantal-taken').textContent = length(taken);
+    document.getElementById('aantal-taken').textContent = taken.length;
 }
 
 function verwijderTaak(index) {
     // TODO: Verwijder taak op specifieke index
-    taken.splice(0,opgeslagenTaken);
+    taken.splice(index);
     
     // TODO: Sla taken op en update UI
-    /* jouw code hier */;
+    document.getElementById('aantal-taken').textContent = taken.length;
     toonTaken();
 }
 
 function wisAlleTaken() {
     if (confirm('Weet je zeker dat je alle taken wilt wissen?')) {
         // TODO: Maak array leeg
-        taken = /* jouw code hier */;
+        taken = [];
         
         // TODO: Verwijder uit localStorage
         localStorage.removeItem("opgeslagenTaak")
