3d7ab00e94163de2650aecab01944b5e56839560
deniaverkade <9024406@student.zadkine.nl>
2025-10-01 13:21:31 +0200

    lijst

--- 3-json/opdracht-01-birthday-reminder/app.js
+++ 3-json/opdracht-01-birthday-reminder/app.js
@@ -1,13 +1,22 @@
-output = document.querySelector("");
-out = "";
+let output = document.querySelector("#list");
+let out = "";
 
-dateToday ="";
-birthdayCounter= 0;
+const dateToday ="03-11";
+const birthdayCounter= 0;
 
 fetch("./birthday.json")
 .then((response) => response.json())
     .then((birthday)=> {
         for (let day of birthday) {
-            if (day.dateOfBirth)
+            if (day.dateOfBirth.startsWith(dateToday)===true) {
+                out += `
+                <img src="${day.picture}">
+                <h2>${day.name}</h2>
+                <p>${day.age} jaar</p>
+                `;
+                console.log("this code works");
+                console.log(out);
+                output.innerHTML = out;
+            }
         }
     })
