8eb2f0680e6a464e74a0d32a1fa756627c322d5a
deniaverkade <9024406@student.zadkine.nl>
2025-10-03 14:14:43 +0200

    a

--- 4-api/opdracht-02-countries/app.js
+++ 4-api/opdracht-02-countries/app.js
@@ -1,1 +1,33 @@
+let outputName = document.querySelector(".nameContent");
+let outputFlag = document.querySelector(".flagContent");
+
+let out1 = "";
+let out2 = "";
+
 fetch(`https://restcountries.com/v3.1/all?fields=name,flags`)
+.then ((response)=> response.json())
+    .then((data) => {
+        for (let i of data) {
+            out1 += `
+            <div class="item1 name${i.name.common}">
+            ${i.name.common}
+            </div>
+            `;
+            outputName.innerHTML= out1;
+            //console.log(out1);
+        }
+    } )
+
+    fetch(`https://restcountries.com/v3.1/all?fields=name,flags`)
+.then ((response)=> response.json())
+    .then((data) => {
+        for (let i of data) {
+            out2 += `
+            <div class="item2 flag${i.name}">
+            <img src="${i.flags.png}">
+            </div>
+            `;
+            outputFlag.innerHTML= out2;
+            //console.log(out2);
+        }
+    } )
